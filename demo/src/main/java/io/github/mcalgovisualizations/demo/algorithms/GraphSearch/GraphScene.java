afe4944df835cae01c3e03d82cabb497eeb71bd5
Lukas <lukasvranic@gmail.com>
2026-06-01 14:46:13 +0200

    added blindness

--- demo/src/main/java/io/github/mcalgovisualizations/demo/algorithms/GraphSearch/GraphScene.java
+++ demo/src/main/java/io/github/mcalgovisualizations/demo/algorithms/GraphSearch/GraphScene.java
@@ -5,20 +5,72 @@ import io.github.mcalgovisualizations.visualization.renderer.LayoutResult;
 import io.github.mcalgovisualizations.visualization.renderer.scene.AbstractScene;
 import io.github.mcalgovisualizations.visualization.renderer.scene.SceneContext;
 import io.github.mcalgovisualizations.visualization.renderer.scene.VillagerPOV;
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.ItemEntity;
+import net.minestom.server.entity.Player;
 import net.minestom.server.instance.Instance;
+import net.minestom.server.potion.Potion;
+import net.minestom.server.potion.PotionEffect;
+import net.minestom.server.timer.Task;
+import net.minestom.server.utils.time.TimeUnit;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GraphScene extends AbstractScene implements VillagerPOV {
     private static final int SEARCHER_SLOT = -100;
     private final List<ItemEntity> extraEntities = new ArrayList<>();
 
+    private Task blindTask = null;
+
     public GraphScene(@NotNull SceneContext context) {
         super(context);
+
+        // Periodically check for players riding villagers in this scene and apply/remove blindness
+        blindTask = MinecraftServer.getSchedulerManager()
+                .buildTask(() -> {
+                    if (instance == null) return;
+
+                    // collect villager entities present in this scene
+                    Set<Entity> villagerEntities = new HashSet<>();
+                    for (var display : displaysBySlot.values()) {
+                        if (display instanceof EntityCreatureDisplay creatureDisplay) {
+                            var e = creatureDisplay.getEntity();
+                            if (e != null) villagerEntities.add(e);
+                        }
+                    }
+
+                    // also consider the special camera target if any
+                    var cam = cameraTarget();
+                    if (cam != null) villagerEntities.add(cam);
+
+                    // for each player in the instance decide if they should have blindness
+                    for (Player player : instance.getPlayers()) {
+                        boolean riding = false;
+                        for (Entity vill : villagerEntities) {
+                            if (vill.getPassengers().contains(player)) {
+                                riding = true;
+                                break;
+                            }
+                        }
+
+                        boolean hasBlind = player.getActiveEffects().stream()
+                                .anyMatch(potion -> potion.potion().effect() == PotionEffect.BLINDNESS);
+
+                        if (riding && !hasBlind) {
+                            Potion blindPotion = new Potion(PotionEffect.BLINDNESS, (byte) 0, Potion.INFINITE_DURATION);
+                            player.addEffect(blindPotion);
+                        } else if (!riding && hasBlind) {
+                            player.removeEffect(PotionEffect.BLINDNESS);
+                        }
+                    }
+                })
+                .repeat(5, TimeUnit.SERVER_TICK)
+                .schedule();
     }
 
     public Instance getInstance() {
@@ -48,6 +100,11 @@ public class GraphScene extends AbstractScene implements VillagerPOV {
 
     @Override
     public void cleanUp() {
+        if (blindTask != null) {
+            blindTask.cancel();
+            blindTask = null;
+        }
+
         for (ItemEntity entity : extraEntities) {
             entity.remove();
         }
